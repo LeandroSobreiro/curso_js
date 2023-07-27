c052e8f23d11b44c5e0b75e8477d2c0064cfe265
LeandroSobreiro <133071578+LeandroSobreiro@users.noreply.github.com>
2023-07-27 19:08:44 -0300

    primeiro

--- treino _coletando dados/javascript.js
+++ treino _coletando dados/javascript.js
@@ -2,5 +2,11 @@
 function enviando(){
     var nome = document.getElementById('nome').value;
     var res = document.getElementById('res');
+    
+   if( nome == ""){
+    window.alert('Erro digite novamente...' )
+   }else{
     res.innerHTML = `Ola ${nome}`
+   }
+
 }
