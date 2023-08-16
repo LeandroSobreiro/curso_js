40faed31e0c57e01e83494129d7549f25f51a37d
LeandroSobreiro <133071578+LeandroSobreiro@users.noreply.github.com>
2023-08-15 21:29:02 -0300

    alinhando

--- ex017/script.js
+++ ex017/script.js
@@ -1,21 +1,24 @@
+// funcao contar pegando valores html
 function contar(){
     let ini = document.getElementById('txti');
     let fim = document.getElementById('txtf');
     let passo = document.getElementById('txtp');
     let res = document.getElementById('res');
     
-
+// condicao se valores iguais a 0 ou nao ter valor
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('[Error] Fantam dados')
     }else{
         window.alert('Vamos contar');
     
         
-   
+   // res.innerHTML = imprime uma msg
     res.innerHTML = 'Contando: <br> '
+    // pegando os valores do input e trasformando em numero
     let i = Number(ini.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
+    
     if(i < f) {
         // Contagem Crescente
         for (i; i <= f; i += p) {
@@ -26,7 +29,6 @@ function contar(){
         for(i; i >= f; i -= p) {
             res.innerHTML += ` ${i} \u{1F449} `
         }
-}
-}
-
+     }
+  }
 }
